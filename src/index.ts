5645b2ba7da1041ddfd36024ba97cfdb6221c297
diaszano <luucas.dsantos@gmail.com>
2023-05-23 22:14:14 -0300

    desenvolvimento: Em andamento.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,39 @@
 import 'dotenv/config';
+import telegram_token from './app/config/Telegram';
+import { Context, Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
 import logger from './app/libs/Logger';
-import cache from './app/libs/Cache';
+import { getId, getName } from './app/tools/telegram/User';
+import User from './app/repository/User';
 
-logger.warn('oi');
-const a = cache.has('ping');
+async function run(): Promise<void> {
+  const telegram = new Telegraf(telegram_token);
 
-console.log(a);
+  telegram.use(async (ctx: Context, next): Promise<void> => {
+    const name: string = getName(ctx);
+    const telegram_id: number | undefined = getId(ctx);
+
+    if (telegram_id) {
+      await User.findOrCreate({ name, telegram_id });
+    }
+
+    const startTime: number = Date.now();
+    await next();
+    const endTime: number = Date.now();
+
+    const elapsedTime: number = (endTime - startTime) / 1000;
+
+    logger.info(
+      `O tempo total para responder o usuário ${name} foi de ${elapsedTime} segundos.`,
+    );
+  });
+  telegram.on(message('text'), async (ctx): Promise<void> => {
+    await ctx.reply(`Oii ${ctx.from?.username}`);
+  });
+
+  await telegram.launch();
+  process.once('SIGINT', (): void => telegram.stop('SIGINT'));
+  process.once('SIGTERM', (): void => telegram.stop('SIGTERM'));
+}
+
+run().then();
